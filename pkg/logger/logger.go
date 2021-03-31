efa11b1b4e44621ed514b8db87cf9e77384efac1
fanzhuo <henbuhai@hotmail.com>
2021-03-31 13:55:09 +0800

    参数校验

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -156,6 +156,13 @@ func (l *Logger) Fatalf(format string, v ...interface{}) {
 	l.Output(LevelFatal, fmt.Sprintf(format, v...))
 }
 
+func (l *Logger) Error(v ...interface{}) {
+	l.Output(LevelFatal, fmt.Sprint(v...))
+}
+
+func (l *Logger) Errorf(format string, v ...interface{}) {
+	l.Output(LevelError, fmt.Sprintf(format, v...))
+}
 
 
 
