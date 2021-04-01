a14d9d8c07278e5f458304ab8ebd737a7f752dcb
fanzhuo <fans1992@qq.com>
2021-04-02 01:01:37 +0800

    标签管理2

--- internal/routers/api/v1/tag.go
+++ internal/routers/api/v1/tag.go
@@ -2,6 +2,7 @@ package v1
 
 import (
 	"blog-service/global"
+	"blog-service/internal/service"
 	"blog-service/pkg/app"
 	"blog-service/pkg/errcode"
 	"github.com/gin-gonic/gin"
@@ -29,20 +30,15 @@ func (t Tag) Get(c *gin.Context) {
 // @Failure 500 {object} errcode.Error "内部错误"
 // @Router /api/v1/tags [get]
 func (t Tag) List(c *gin.Context) {
-	param := struct {
-		Name  string `form:"name" binding:"max=100"`
-		State uint8  `form:"state,default=1" binding:"oneof=0 1"`
-	}{}
+	param := service.TagListRequest{}
 	response := app.NewResponse(c)
 	valid, errs := app.BindAndValid(c, &param)
 	if !valid {
 		global.Logger.Errorf("app.BindAndValid errs: %v", errs)
 		response.ToErrorResponse(errcode.InvalidParams.WithDetails(errs.Errors()...))
-		return
 	}
+	
 
-	response.ToResponse(gin.H{})
-	return
 }
 
 // @Summary 新增标签
