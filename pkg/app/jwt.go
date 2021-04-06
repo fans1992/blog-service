748917c6741be4e760e74f75c9c574ce4fc5fb71
fanzhuo <fans1992@qq.com>
2021-04-07 00:32:59 +0800

    初始化jwt

---
+++ pkg/app/jwt.go
@@ -0,0 +1,35 @@
+package app
+
+import (
+	"blog-service/global"
+	"blog-service/pkg/util"
+	"github.com/dgrijalva/jwt-go"
+	"time"
+)
+
+type Claims struct {
+	AppKey string `json:"app_key"`
+	AppSecret string `json:"app_secret"`
+	jwt.StandardClaims
+}
+
+func GetJWTSecret() []byte {
+	return []byte(global.JWTSetting.Secret)
+}
+
+func GenerateToken(appKey, appSecret string) (string, error) {
+	nowTime := time.Now()
+	expireTime := nowTime.Add(global.JWTSetting.Expire)
+	claims := Claims{
+		AppKey:    util.EncodeMD5(appKey),
+		AppSecret: util.EncodeMD5(appSecret),
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expireTime.Unix(),
+			Issuer:    global.JWTSetting.Issuer,
+		},
+	}
+
+	tokenClaims := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	token, err := tokenClaims.SignedString(GetJWTSecret())
+	return token, err
+}
